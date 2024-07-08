a60161c9ffb9eeba004702f09157184bca52b7f9
Elchin Aghayev <Elchin@code.local>
2024-07-08 19:13:27 +0400

    project added

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Welcome "+name);
+    }
+}
